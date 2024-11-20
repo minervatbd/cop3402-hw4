d3fc2592b0edeb15b08c2c70a5710e4767097c1f
Lex Matthews <tanukiguernica@gmail.com>
2024-11-20 15:18:12 -0500

    filled out header declarations and began work on first iteration of the code generator

--- gen_code.c
+++ gen_code.c
@@ -1,14 +1,41 @@
 #include "gen_code.h"
 
 // Initialize the code generator
-extern void gen_code_initialize()
+void gen_code_initialize()
 {
-
+    //initalize literal table (call module function)
 }
 
 // Requires: bf is open for writing
 // Generate code for prog into bf
-extern void gen_code_program(BOFFILE bf, block_t prog)
+void gen_code_program(BOFFILE bf, block_t prog)
 {
+    code_seq mainSeq = code_seq_empty();
+    code_seq_concat(&mainSeq, gen_code_varDecls(prog.var_decls));
+    
+}
+
+
+code_seq gen_code_statement(stmt_t statement){
 
 }
+
+code_seq gen_code_varDecls(var_decls_t varDecls){
+
+}
+
+code_seq gen_code_ident(ident_t identifier){
+
+}
+
+code_seq gen_code_proc(proc_decl_t procedures){
+
+}
+
+
+extern void gen_code_final_output(code mainSeq){
+
+}
+
+
+
--- gen_code.h
+++ gen_code.h
@@ -3,6 +3,8 @@
 
 #include "bof.h"
 #include "ast.h"
+#include "code.h"
+#include "code_seq.h"
 
 // Initialize the code generator
 extern void gen_code_initialize();
@@ -10,5 +12,15 @@ extern void gen_code_initialize();
 // Requires: bf is open for writing
 // Generate code for prog into bf
 extern void gen_code_program(BOFFILE bf, block_t prog);
+
+//code generation
+code gen_code_statement(stmt_t statement);
+code gen_code_varDecls(var_decl_t varDecls);
+code gen_code_ident(ident_t identifier);
+code gen_code_proc(proc_decl_t procedures);
+
+//ending functions
+extern void gen_code_makeHeader(code mainSeq);
+extern void gen_code_final_output(code mainSeq);
 
 #endif
