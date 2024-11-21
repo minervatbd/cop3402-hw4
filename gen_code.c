922718d82919062c5913526173d28b80d57dcff5
Lex Matthews <tanukiguernica@gmail.com>
2024-11-21 15:29:04 -0500

    recursive transversal of AST complete. Still need to imlement large bulk of actual code generation

--- gen_code.c
+++ gen_code.c
@@ -11,13 +11,31 @@ void gen_code_initialize()
 void gen_code_program(BOFFILE bf, block_t prog)
 {
     code_seq mainSeq = code_seq_empty();
-    code_seq_concat(&mainSeq, gen_code_varDecls(prog.var_decls));
-    
+    //Todo: handle constant declarations (needs literal table module)
+
+//build code sequence recursively
+    code_seq_concat(&mainSeq, gen_code_block(prog));
+
+//Todo: construct bof header from finished code sequence
+
+//output
+    gen_code_final_output(&mainSeq);
 }
 
+code_seq gen_code_block(block_t block){
+    code_seq ret;
+    code_seq_concat(&ret, gen_code_varDecls(block.var_decls));
 
-code_seq gen_code_statement(stmt_t statement){
+//if statements exist within block, recursively parse stmts ast into code
+    if(block.stmts.stmts_kind != empty_stmts_e){
+        stmt_t* curr= block.stmts.stmt_list.start;
+    //iterate through linked list of statements
+        while(curr!=NULL){
+            code_seq_concat(&ret, gen_code_statement(*curr));
+        }
+    }
 
+    return ret;
 }
 
 code_seq gen_code_varDecls(var_decls_t varDecls){
@@ -26,23 +44,19 @@ code_seq gen_code_varDecls(var_decls_t varDecls){
 
 //iterate through linked list of variable declarations to build code seq
     while(curr!=NULL){
-        code_seq_concat(&ret, gen_code_varDecl(curr));
+        code_seq_concat(&ret, gen_code_identList(curr->ident_list));
         curr = curr->next;
     }
 
     return ret;
 }
-
-code_seq gen_code_varDecl(var_decl_t* varDecl){
-    return gen_code_identList(varDecl->ident_list);
-}
-
 code_seq gen_code_identList(ident_list_t idList){
     code_seq ret;
     ident_t* curr = idList.start;
-
+    
     while(curr!=NULL){
-        
+    //for every declared variable, allocate space for it on runtime stack (within current AR)
+        code_seq_concat(&ret, code_utils_allocate_stack_space(1));
         curr->next;
     }
 
@@ -50,12 +64,51 @@ code_seq gen_code_identList(ident_list_t idList){
 }
 
 code_seq gen_code_proc(proc_decl_t procedures){
+//Todo
+}
 
+code_seq gen_code_statement(stmt_t statement){
+    code_seq ret;
+//based on type of statement, peform the corresponding operations
+    switch(statement.stmt_kind){
+        case(assign_stmt):{
+        assign_stmt_t data = statement.data.assign_stmt;
+        //Todo: copy value to specified register (cpw)
+            //code_seq_concat(&ret, code_cpw())
+        }
+        
+        case(call_stmt):{
+        //Todo: 
+        }
+
+        case(if_stmt):{
+        //Todo:
+        }
+
+        case(while_stmt):{
+        //Todo:
+        }
+        
+        case(read_stmt):{
+        //Todo:
+        }
+        
+        case(print_stmt):{
+        //Todo:
+        }
+        
+        case(block_stmt):{
+        //Todo:
+            code_seq_concat(&ret, gen_code_block(*statement.data.block_stmt.block));
+        }
+
+        default: bail_with_error("Non-statement AST was provided as a statement");
+    }
 }
 
 
-extern void gen_code_final_output(code mainSeq){
-
+extern void gen_code_final_output(code_seq* mainSeq){
+//Todo
 }
 
 
--- gen_code.h
+++ gen_code.h
@@ -3,8 +3,10 @@
 
 #include "bof.h"
 #include "ast.h"
+#include "utilities.h"
 #include "code.h"
 #include "code_seq.h"
+#include "code_utils.h"
 
 // Initialize the code generator
 extern void gen_code_initialize();
@@ -16,12 +18,11 @@ extern void gen_code_program(BOFFILE bf, block_t prog);
 //code generation
 code_seq gen_code_statement(stmt_t statement);
 code_seq gen_code_varDecls(var_decl_t varDecls);
-code_seq gen_code_varDecl(var_decl_t* varDecl);
 code_seq gen_code_identList(ident_list_t idList);
 code_seq gen_code_proc(proc_decl_t procedures);
 
 //ending functions
-extern void gen_code_makeHeader(code mainSeq);
-extern void gen_code_final_output(code mainSeq);
+extern void gen_code_makeHeader(BOFFILE bf, code_seq* mainSeq);
+extern void gen_code_final_output(code_seq* mainSeq);
 
 #endif
