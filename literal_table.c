b124fa9ff1929f86ff4897c3459fc5cda5a37154
Minerva <minervatbd>
2024-11-23 21:05:45 -0500

    some blank functions

--- literal_table.c
+++ literal_table.c
@@ -3,5 +3,35 @@
 // initialize the literal table
 extern void literal_table_initialize()
 {
+    
+}
+
+extern int literal_table_empty()
+{
+
+}
+
+extern int literal_table_full()
+{
+
+}
+
+extern int literal_table_size()
+{
+
+}
+
+extern int literal_table_find_offset(const char *target, word_type value)
+{
+
+}
+
+extern int literal_table_present(const char *sought, word_type value)
+{
+
+}
+
+extern int literal_table_lookup(const char *sought, word_type value)
+{
 
 }
--- literal_table.h
+++ literal_table.h
@@ -1,6 +1,19 @@
 #ifndef LITERAL_TABLE_H
 #define LITERAL_TABLE_H
+#include "machine_types.h"
 
 extern void literal_table_initialize();
+
+extern int literal_table_empty();
+
+extern int literal_table_full();
+
+extern int literal_table_size();
+
+extern int literal_table_find_offset(const char *target, word_type value);
+
+extern int literal_table_present(const char *sought, word_type value);
+
+extern int literal_table_lookup(const char *sought, word_type value);
 
 #endif
