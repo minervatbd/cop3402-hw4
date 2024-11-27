11e7cbd721089f4b85ad07fd70a163e070e2e182
joann101 <125590213+joann101@users.noreply.github.com>
2024-11-27 16:29:33 -0500

    Update gen_code.c
    update on reading header functions

--- gen_code.c
+++ gen_code.c
@@ -1,9 +1,41 @@
+#include <string.h>
 #include "gen_code.h"
+
+#define STACK_SPACE 4096
 
 // Initialize the code generator
 void gen_code_initialize()
 {
     literal_table_initialize();
+}
+
+// Requires: bf if open for writing in binary
+// and prior to this scope checking and type checking have been done.
+// Write all the instructions in cs to bf in order
+void gen_code_output_seq(BOFFILE bf, code_seq cs)
+{
+    while (!code_seq_is_empty(cs)) {
+	bin_instr_t inst = code_seq_first(cs)->instr;
+	instruction_write_bin_instr(bf, inst);
+	cs = code_seq_rest(cs);
+    }
+}
+
+// Return a header appropriate for the give code
+BOFHeader gen_code_makeHeader(code_seq* mainSeq)
+{
+    BOFHeader ret;
+    strncpy(ret.magic, "FBF", 4); // idk
+    ret.text_start_address = 0;
+    ret.text_length = code_seq_size(*mainSeq) * BYTES_PER_WORD;
+    int dsa = MAX(ret.text_length, 1024) + BYTES_PER_WORD;
+    ret.data_start_address = dsa;
+    ret.data_length = literal_table_size() * BYTES_PER_WORD;
+    int sba = dsa
+	+ ret.data_start_address
+	+ ret.data_length + STACK_SPACE;
+    ret.stack_bottom_addr = sba;
+    return ret;
 }
 
 // Requires: bf is open for writing
@@ -19,7 +51,7 @@ void gen_code_program(BOFFILE bf, block_t prog)
 //Todo: construct bof header from finished code sequence
 
 //output
-    gen_code_final_output(&mainSeq);
+    gen_code_final_output(bf, &mainSeq);
 }
 
 code_seq gen_code_block(block_t block){
@@ -56,6 +88,7 @@ code_seq gen_code_varDecls(var_decls_t varDecls){
 
     return ret;
 }
+
 code_seq gen_code_identList(ident_list_t idList){
     code_seq ret;
     ident_t* curr = idList.start;
@@ -122,31 +155,41 @@ code_seq gen_code_expr(expr_t expression){
     code_seq ret;
 
     switch(expression.expr_kind){
-        case(expr_bin):{
-
-        }
-        case(expr_negated):{
-            
-        }
-        case(expr_ident):{
-            
-        }
-        case(expr_number):{
-            
-        }
+        case(expr_bin):
+            //return gen_code_binary_op_expr(expression.data.binary);
+            break;
+        case(expr_negated):
+            //return gen_code_negated(expression.data.ident);
+	        break; 
+        
+        case(expr_ident):
+            //return gen_code_ident(expression.data.ident);
+	        break;           
+        
+        case(expr_number):
+            //return gen_code_number(exp.data.number);
+	        break;
+        
         default:
+            bail_with_error("Unexpected expr_kind_e (%d) in gen_code_expr", expression.expr_kind);
+	        break;
     }
-
+    return code_seq_empty();
 }
 
 
-extern void gen_code_final_output(code_seq* mainSeq){
-//Todo
+void gen_code_final_output(BOFFILE bf, code_seq* mainSeq)
+{
+    BOFHeader bfh = gen_code_makeHeader(mainSeq);
+    bof_write_header(bf, bfh);
+    gen_code_output_seq(bf, *mainSeq);
+    gen_code_output_literals(bf);
+    bof_close(bf);
 }
 
 
 // call this for literals at the end of the final output
-static void gen_code_output_literals(BOFFILE bof)
+void gen_code_output_literals(BOFFILE bof)
 {
     // flip this bool for forwards/backwards output
     int backwards = 0;
